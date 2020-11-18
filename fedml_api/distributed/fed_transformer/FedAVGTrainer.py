559303cd2eac313f2c11c08742b3b34a9c989978
Chaoyang He <chaoyanghe.com@gmail.com>
2020-11-18 03:44:54 -0800

    add fed transformer

---
+++ fedml_api/distributed/fed_transformer/FedAVGTrainer.py
@@ -0,0 +1,86 @@
+import logging
+
+import torch
+from torch import nn
+
+from fedml_api.distributed.fed_transformer.utils import transform_tensor_to_list, WarmupCosineSchedule, \
+    WarmupLinearSchedule
+
+
+class FedAVGTrainer(object):
+    def __init__(self, client_index, train_data_local_dict, train_data_local_num_dict, train_data_num, device, model,
+                 args):
+        self.client_index = client_index
+        self.train_data_local_dict = train_data_local_dict
+        self.train_data_local_num_dict = train_data_local_num_dict
+        self.all_train_data_num = train_data_num
+        self.train_local = self.train_data_local_dict[client_index]
+        self.local_sample_number = self.train_data_local_num_dict[client_index]
+
+        self.device = device
+        self.args = args
+        self.model = model
+        # logging.info(self.model)
+        self.model.to(self.device)
+        self.criterion = nn.CrossEntropyLoss().to(self.device)
+        if self.args.client_optimizer == "sgd":
+            self.optimizer = torch.optim.SGD(model.parameters(),
+                                             lr=args.learning_rate,
+                                             momentum=0.9,
+                                             weight_decay=args.wd)
+        else:
+            self.optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, self.model.parameters()),
+                                              lr=self.args.lr,
+                                              weight_decay=self.args.wd, amsgrad=True)
+
+        if args.decay_type == "cosine":
+            self.scheduler = WarmupCosineSchedule(self.optimizer, warmup_steps=args.warmup_steps,
+                                                  t_total=args.comm_round)
+        else:
+            self.scheduler = WarmupLinearSchedule(self.optimizer, warmup_steps=args.warmup_steps,
+                                                  t_total=args.comm_round)
+
+    def update_model(self, weights):
+        # logging.info("update_model. client_index = %d" % self.client_index)
+        self.model.load_state_dict(weights)
+
+    def update_dataset(self, client_index):
+        self.client_index = client_index
+        self.train_local = self.train_data_local_dict[client_index]
+        self.local_sample_number = self.train_data_local_num_dict[client_index]
+
+    def train(self):
+        self.model.to(self.device)
+        # change to train mode
+        self.model.train()
+
+        epoch_loss = []
+        for epoch in range(self.args.epochs):
+            batch_loss = []
+            for batch_idx, (x, labels) in enumerate(self.train_local):
+                # logging.info(images.shape)
+                x, labels = x.to(self.device), labels.to(self.device)
+                self.optimizer.zero_grad()
+                log_probs, _ = self.model(x)
+                loss = self.criterion(log_probs, labels)
+                loss.backward()
+                # according to ViT paper, all fine-tuned tasks do gradient clipping at global norm 1.
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
+                self.optimizer.step()
+                self.scheduler.step()
+                batch_loss.append(loss.item())
+                if len(batch_loss) > 0:
+                    epoch_loss.append(sum(batch_loss) / len(batch_loss))
+                    logging.info(
+                        '(client {}. Local Training Epoch: {}\tBatch:{}/{}\tLoss: {:.6f}'.format(self.client_index,
+                                                                                                 epoch,
+                                                                                                 batch_idx,
+                                                                                                 len(self.train_local),
+                                                                                                 sum(epoch_loss) / len(
+                                                                                                     epoch_loss)))
+        weights = self.model.cpu().state_dict()
+
+        # transform Tensor to list
+        if self.args.is_mobile == 1:
+            weights = transform_tensor_to_list(weights)
+        return weights, self.local_sample_number
---
+++ fedml_experiments/distributed/fed_transformer/README.md
@@ -0,0 +1,59 @@
+## Installation
+http://doc.fedml.ai/#/installation-distributed-computing
+
+## Run Experiments
+
+## ResNet56 Federated Training
+
+#### CIFAR10
+train on IID dataset 
+```
+sh run_fedavg_distributed_pytorch.sh 2 2 1 8 transformer homo 100 20 64 0.001 cifar10 "./../../../data/cifar10" 0
+
+##run on background
+nohup sh run_fedavg_distributed_pytorch.sh 10 10 1 8 resnet56 homo 100 20 64 0.001 cifar10 "./../../../data/cifar10" 0 > ./fedavg-resnet-homo-cifar10.txt 2>&1 &
+```
+
+train on non-IID dataset
+```
+sh run_fedavg_distributed_pytorch.sh 10 10 1 8 resnet56 hetero 100 20 64 0.001 cifar10 "./../../../data/cifar10" 0
+
+##run on background
+nohup sh run_fedavg_distributed_pytorch.sh 10 10 1 8 resnet56 hetero 100 20 64 0.001 cifar10 "./../../../data/cifar10" 0 > ./fedavg-resnet-hetero-cifar10.txt 2>&1 &
+```
+
+
+#### CIFAR100
+train on IID dataset 
+```
+sh run_fedavg_distributed_pytorch.sh 10 10 1 8 resnet56 homo 100 20 64 0.001 cifar100 "./../../../data/cifar100" 0
+
+##run on background
+nohup sh run_fedavg_distributed_pytorch.sh 10 10 1 8 resnet56 homo 100 20 64 0.001 cifar100 "./../../../data/cifar100" 0 > ./fedavg-resnet-homo-cifar100.txt 2>&1 &
+```
+
+train on non-IID dataset
+```
+sh run_fedavg_distributed_pytorch.sh 10 10 1 8 resnet56 hetero 100 20 64 0.001 cifar100 "./../../../data/cifar100" 0
+
+##run on background
+nohup sh run_fedavg_distributed_pytorch.sh 10 10 1 8 resnet56 hetero 100 20 64 0.001 cifar100 "./../../../data/cifar100" 0 > ./fedavg-resnet-hetero-cifar100.txt 2>&1 &
+```
+
+
+#### CINIC10
+train on IID dataset 
+```
+sh run_fedavg_distributed_pytorch.sh 10 10 1 8 resnet56 homo 100 20 64 0.001 cinic10 "./../../../data/cinic10" 0
+
+##run on background
+nohup sh run_fedavg_distributed_pytorch.sh 10 10 1 8 resnet56 homo 100 20 64 0.001 cinic10 "./../../../data/cinic10" 0 > ./fedavg-resnet-homo-cinic10.txt 2>&1 &
+```
+
+train on non-IID dataset
+```
+sh run_fedavg_distributed_pytorch.sh 10 10 1 8 resnet56 hetero 100 20 64 0.001 cinic10 "./../../../data/cinic10" 0
+
+##run on background
+nohup sh run_fedavg_distributed_pytorch.sh 10 10 1 8 resnet56 hetero 100 20 64 0.001 cinic10 "./../../../data/cinic10" 0 > ./fedavg-resnet-hetero-cinic10.txt 2>&1 &
+```
