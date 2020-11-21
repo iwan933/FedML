2f947c5605411fe0a1e4eda3889e15c1ea8e92e6
Chaoyang He <chaoyanghe.com@gmail.com>
2020-11-21 12:39:27 -0800

    change the transforms

--- fedml_api/data_preprocessing/cifar10/data_loader.py
+++ fedml_api/data_preprocessing/cifar10/data_loader.py
@@ -298,16 +298,16 @@ def load_cifar10_centralized_training_for_vit(args):
 
     transform_train = transforms.Compose([
         transforms.RandomResizedCrop((args.img_size, args.img_size), scale=(0.05, 1.0)),
-        # transforms.RandomHorizontalFlip(),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-        # transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+        # transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
     ])
     transform_test = transforms.Compose([
         transforms.Resize((args.img_size, args.img_size)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-        # transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+        #transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
     ])
 
     if args.dataset == "cifar10":
--- fedml_experiments/centralized/fed_transformer/sweep.sh
+++ fedml_experiments/centralized/fed_transformer/sweep.sh
@@ -1,5 +1,7 @@
-python ./main_vit_cifar10.py --lr 0.001
-python ./main_vit_cifar10.py --lr 0.1
+# the best lr = 0.001, test accuracy is = 0.9661, training accuracy is 0.8654
+
 python ./main_vit_cifar10.py --lr 0.01
+python ./main_vit_cifar10.py --lr 0.001
 python ./main_vit_cifar10.py --lr 0.0001
+python ./main_vit_cifar10.py --lr 0.1
 python ./main_vit_cifar10.py --lr 0.00001
