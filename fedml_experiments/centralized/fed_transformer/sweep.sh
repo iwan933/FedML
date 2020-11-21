46e859e4555e24d1706b313bcf836dc805130316
Chaoyang He <chaoyanghe.com@gmail.com>
2020-11-21 03:19:41 -0800

    Update sweep.sh

--- fedml_experiments/centralized/fed_transformer/sweep.sh
+++ fedml_experiments/centralized/fed_transformer/sweep.sh
@@ -1,4 +1,5 @@
+python ./main_vit_cifar10.py --lr 0.001
 python ./main_vit_cifar10.py --lr 0.1
 python ./main_vit_cifar10.py --lr 0.01
-python ./main_vit_cifar10.py --lr 0.001
+python ./main_vit_cifar10.py --lr 0.0001
 python ./main_vit_cifar10.py --lr 0.00001
