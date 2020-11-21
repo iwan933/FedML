9261db7a646e4eb38ed6a8dd7f9f279e76864408
Chaoyang He <chaoyanghe.com@gmail.com>
2020-11-21 01:04:08 -0800

    debug

--- fedml_experiments/centralized/README.md
+++ fedml_experiments/centralized/README.md
@@ -1,6 +1,13 @@
 
 
 # PyTorch DDP demo
+```
 # sh run_ddp.sh 8 2 0 192.168.11.1 11111
 nohup sh run_ddp.sh 8 2 0 192.168.11.1 11111 > ./machine1.txt 2>&1 &
 nohup sh run_ddp.sh 8 2 1 192.168.11.1 11111 > ./machine2.txt 2>&1 &
+```
+
+```
+# kill all processes
+kill $(ps aux | grep "ddp_demo.py" | grep -v grep | awk '{print $2}')
+```
--- fedml_experiments/centralized/ddp_demo.py
+++ fedml_experiments/centralized/ddp_demo.py
@@ -9,13 +9,6 @@ import torch.optim as optim
 from torch.nn.parallel import DistributedDataParallel as DDP
 
 
-def setup(rank, world_size):
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
-
-    # initialize the process group
-    dist.init_process_group(backend="nccl", init_method="env://", rank=rank, world_size=world_size)
-
 
 def cleanup():
     dist.destroy_process_group()
@@ -39,23 +32,22 @@ def run_demo(demo_fn, world_size):
              join=True)
 
 
-def demo_basic(rank, world_size):
-    print(f"Running basic DDP example on rank {rank}.")
-    setup(rank, world_size)
+def demo_basic(local_rank):
+    print(f"Running basic DDP example on local rank {local_rank}.")
 
     # create model and move it to GPU with id rank
-    model = ToyModel().to(rank)
-    ddp_model = DDP(model, device_ids=[rank])
+    model = ToyModel().to(local_rank)
+    ddp_model = DDP(model, device_ids=[local_rank])
 
     loss_fn = nn.MSELoss()
     optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
 
     optimizer.zero_grad()
     outputs = ddp_model(torch.randn(20, 10))
-    labels = torch.randn(20, 5).to(rank)
+    labels = torch.randn(20, 5).to(local_rank)
     loss = loss_fn(outputs, labels)
     loss.backward()
-    print("rank=%d, loss=%f" % (rank, loss))
+    print("rank=%d, loss=%f" % (local_rank, loss))
     optimizer.step()
 
     cleanup()
@@ -72,10 +64,16 @@ if __name__ == "__main__":
     # This the global rank: 0, 1, 2, ..., 15
     global_rank = int(os.environ['RANK'])
     print("int(os.environ['RANK']) = %d" % global_rank)
+
+    # This the globak world_size
+    world_size = int(os.environ['WORLD_SIZE'])
+    print("world_size = %d" % world_size)
+
     gpu_device_id = global_rank % gpu_per_node
     print("gpu_device_id = " + str(gpu_device_id))
 
-    world_size = int(os.environ['WORLD_SIZE'])
-    print("world_size = %d" % world_size)
-    demo_basic(args.local_rank, world_size)
-    # run_demo(demo_basic, 8)
+    # initialize the process group
+    dist.init_process_group(backend="nccl", init_method="env://", rank=args.local_rank, world_size=world_size)
+
+    demo_basic(args.local_rank)
+
