9077bb9952106e9b0d4c6d0e6f57922c579c69d5
Chaoyang He <chaoyanghe.com@gmail.com>
2020-11-22 13:28:52 -0800

    add

--- fedml_experiments/distributed/fed_transformer/sweep-hetero-cifar10.sh
+++ fedml_experiments/distributed/fed_transformer/sweep-hetero-cifar10.sh
@@ -11,21 +11,21 @@
 
 # sweep-hetero
 # nohup sh sweep-hetero-cifar10.sh > sweep-hetero-cifar10.txt 2>&1 &
-sh run_fed_transformer.sh 10 10 1 4 transformer hetero 100 1 64 0.0001 cifar10 "./../../../data/cifar10" 0 0
+sh run_fed_transformer.sh 10 10 1 8 transformer hetero 100 1 64 0.0001 cifar10 "./../../../data/cifar10" 0 0
 
-sh run_fed_transformer.sh 10 10 1 4 transformer hetero 100 1 64 0.0003 cifar10 "./../../../data/cifar10" 0 0
+sh run_fed_transformer.sh 10 10 1 8 transformer hetero 100 1 64 0.0003 cifar10 "./../../../data/cifar10" 0 0
 
-sh run_fed_transformer.sh 10 10 1 4 transformer hetero 100 1 64 0.001 cifar10 "./../../../data/cifar10" 0 0
+sh run_fed_transformer.sh 10 10 1 8 transformer hetero 100 1 64 0.001 cifar10 "./../../../data/cifar10" 0 0
 
-sh run_fed_transformer.sh 10 10 1 4 transformer hetero 100 1 64 0.003 cifar10 "./../../../data/cifar10" 0 0
+sh run_fed_transformer.sh 10 10 1 8 transformer hetero 100 1 64 0.003 cifar10 "./../../../data/cifar10" 0 0
 
-sh run_fed_transformer.sh 10 10 1 4 transformer hetero 100 1 64 0.1 cifar10 "./../../../data/cifar10" 0 0
+sh run_fed_transformer.sh 10 10 1 8 transformer hetero 100 1 64 0.1 cifar10 "./../../../data/cifar10" 0 0
 
-sh run_fed_transformer.sh 10 10 1 4 transformer hetero 100 1 64 0.3 cifar10 "./../../../data/cifar10" 0 0
+sh run_fed_transformer.sh 10 10 1 8 transformer hetero 100 1 64 0.3 cifar10 "./../../../data/cifar10" 0 0
 
-sh run_fed_transformer.sh 10 10 1 4 transformer hetero 100 1 64 0.03 cifar10 "./../../../data/cifar10" 0 0
+sh run_fed_transformer.sh 10 10 1 8 transformer hetero 100 1 64 0.03 cifar10 "./../../../data/cifar10" 0 0
 
-sh run_fed_transformer.sh 10 10 1 4 transformer hetero 100 1 64 0.01 cifar10 "./../../../data/cifar10" 0 0
+sh run_fed_transformer.sh 10 10 1 8 transformer hetero 100 1 64 0.01 cifar10 "./../../../data/cifar10" 0 0
 
 
 
--- fedml_experiments/distributed/fed_transformer/sweep-homo-cifar100.sh
+++ fedml_experiments/distributed/fed_transformer/sweep-homo-cifar100.sh
@@ -1,18 +1,18 @@
 
 # sweep-homo
 # nohup sh sweep-homo-cifar100.sh > sweep-homo-cifar100.txt 2>&1 &
-sh run_fed_transformer.sh 10 10 1 4 transformer homo 100 1 64 0.0001 cifar100 "./../../../data/cifar100" 0 0
+sh run_fed_transformer.sh 10 10 1 8 transformer homo 100 1 64 0.0001 cifar100 "./../../../data/cifar100" 0 0
 
-sh run_fed_transformer.sh 10 10 1 4 transformer homo 100 1 64 0.0003 cifar100 "./../../../data/cifar100" 0 0
+sh run_fed_transformer.sh 10 10 1 8 transformer homo 100 1 64 0.0003 cifar100 "./../../../data/cifar100" 0 0
 
-sh run_fed_transformer.sh 10 10 1 4 transformer homo 100 1 64 0.001 cifar100 "./../../../data/cifar100" 0 0
+sh run_fed_transformer.sh 10 10 1 8 transformer homo 100 1 64 0.001 cifar100 "./../../../data/cifar100" 0 0
 
-sh run_fed_transformer.sh 10 10 1 4 transformer homo 100 1 64 0.003 cifar100 "./../../../data/cifar100" 0 0
+sh run_fed_transformer.sh 10 10 1 8 transformer homo 100 1 64 0.003 cifar100 "./../../../data/cifar100" 0 0
 
-sh run_fed_transformer.sh 10 10 1 4 transformer homo 100 1 64 0.1 cifar100 "./../../../data/cifar100" 0 0
+sh run_fed_transformer.sh 10 10 1 8 transformer homo 100 1 64 0.1 cifar100 "./../../../data/cifar100" 0 0
 
-sh run_fed_transformer.sh 10 10 1 4 transformer homo 100 1 64 0.3 cifar100 "./../../../data/cifar100" 0 0
+sh run_fed_transformer.sh 10 10 1 8 transformer homo 100 1 64 0.3 cifar100 "./../../../data/cifar100" 0 0
 
-sh run_fed_transformer.sh 10 10 1 4 transformer homo 100 1 64 0.03 cifar100 "./../../../data/cifar100" 0 0
+sh run_fed_transformer.sh 10 10 1 8 transformer homo 100 1 64 0.03 cifar100 "./../../../data/cifar100" 0 0
 
-sh run_fed_transformer.sh 10 10  1 4 transformer homo 100 1 64 0.01 cifar100 "./../../../data/cifar100" 0 0
+sh run_fed_transformer.sh 10 10  1 8 transformer homo 100 1 64 0.01 cifar100 "./../../../data/cifar100" 0 0
