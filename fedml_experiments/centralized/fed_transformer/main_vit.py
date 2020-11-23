759f3d55581e807d57e7cbbf1bcfd22701ceea5c
Chaoyang He <chaoyanghe.com@gmail.com>
2020-11-23 12:19:55 -0800

    Update main_vit.py

--- fedml_experiments/centralized/fed_transformer/main_vit.py
+++ fedml_experiments/centralized/fed_transformer/main_vit.py
@@ -226,6 +226,27 @@ def train_and_eval(model, train_dl, test_dl, args, device):
         eval(epoch, train_data_extracted_features, test_data_extracted_features)
 
 
+def mixup_data(x, y, alpha=1.0, use_cuda=True):
+    '''Returns mixed inputs, pairs of targets, and lambda'''
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha)
+    else:
+        lam = 1
+
+    batch_size = x.size()[0]
+    if use_cuda:
+        index = torch.randperm(batch_size).cuda()
+    else:
+        index = torch.randperm(batch_size)
+
+    mixed_x = lam * x + (1 - lam) * x[index, :]
+    y_a, y_b = y, y[index]
+    return mixed_x, y_a, y_b, lam
+
+
+def mixup_criterion(criterion, pred, y_a, y_b, lam):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="PyTorch DDP Demo")
     parser.add_argument("--local_rank", type=int, default=0)
