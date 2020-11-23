a9847edd7e04a55275f7c1ab64761e9e08ad88f5
Chaoyang He <chaoyanghe.com@gmail.com>
2020-11-22 18:24:26 -0800

    Update sweep-cifar10.sh

--- fedml_experiments/centralized/fed_transformer/sweep-cifar10.sh
+++ fedml_experiments/centralized/fed_transformer/sweep-cifar10.sh
@@ -1,8 +1,5 @@
 # the best lr = 0.001, test accuracy is = 0.9661, training accuracy is 0.8654
 
-python ./main_vit.py --lr 0.1 --dataset cifar10 --data_dir ./../../../data/cifar10
-python ./main_vit.py --lr 0.3 --dataset cifar10 --data_dir ./../../../data/cifar10
-
 # 96.36; 86.27
 python ./main_vit.py --lr 0.01 --dataset cifar10 --data_dir ./../../../data/cifar10
 
@@ -15,15 +12,3 @@ python ./main_vit.py --lr 0.001 --dataset cifar10 --data_dir ./../../../data/cif
 
 # 0.9657
 python ./main_vit.py --lr 0.003 --dataset cifar10 --data_dir ./../../../data/cifar10
-
-# 0.9599
-python ./main_vit.py --lr 0.0001 --dataset cifar10 --data_dir ./../../../data/cifar10
-
-# 0.9621
-python ./main_vit.py --lr 0.0003 --dataset cifar10 --data_dir ./../../../data/cifar10
-
-# 0.9531
-python ./main_vit.py --lr 0.00001 --dataset cifar10 --data_dir ./../../../data/cifar10
-
-# 0.9564
-python ./main_vit.py --lr 0.00003 --dataset cifar10 --data_dir ./../../../data/cifar10
