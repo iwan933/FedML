f490c2a4edf863fb253fd7edca9255d9cad6517b
Chaoyang He <chaoyanghe.com@gmail.com>
2020-11-22 18:36:51 -0800

    Update data_loader.py

--- fedml_api/data_preprocessing/cifar10/data_loader.py
+++ fedml_api/data_preprocessing/cifar10/data_loader.py
@@ -277,8 +277,8 @@ def load_cifar10_centralized_training_for_vit(args):
         transforms.RandomResizedCrop((args.img_size, args.img_size), scale=(0.05, 1.0)),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-        # transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+        # transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
     ])
     transform_test = transforms.Compose([
         transforms.Resize((args.img_size, args.img_size)),
