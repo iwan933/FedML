17da2166d1ccb853696b482dbba812ce8d61f17f
Chaoyang He <chaoyanghe.com@gmail.com>
2020-11-26 13:46:28 -0800

    Update run_centralized_ddp_train.sh

--- fedml_experiments/centralized/fed_transformer/run_centralized_ddp_train.sh
+++ fedml_experiments/centralized/fed_transformer/run_centralized_ddp_train.sh
@@ -5,9 +5,15 @@ NNODE=$2
 NODE_RANK=$3
 MASTER_ADDR=$4
 MASTER_PORT=$5
+DATASET=$6
+DATA_DIR=$7
+LR=$8
 
 python -m torch.distributed.launch \
 --nproc_per_node=$NPROC_PER_NODE --nnodes=$NNODE --node_rank=$NODE_RANK \
 --master_addr $MASTER_ADDR \
 --master_port $MASTER_PORT \
-main_vit_fine_tune.py
+main_vit_fine_tune.py \
+--lr $LR \
+--dataset $DATASET \
+--data_dir $DATA_DIR
