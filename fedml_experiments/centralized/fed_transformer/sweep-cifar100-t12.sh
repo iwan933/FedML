a6182d0ceeb93b2932d9c97b8b96fa264856d587
chaoyanghe <me@chaoyanghe.com>
2020-12-01 00:41:31 -0800

    add

--- fedml_experiments/centralized/fed_transformer/sweep-cifar100-t12.sh
+++ fedml_experiments/centralized/fed_transformer/sweep-cifar100-t12.sh
@@ -1,12 +1,12 @@
 # the best lr = 0.001, test accuracy is = 0.9661, training accuracy is 0.8654
-python ./main_vit_frozen.py --lr 0.03 --dataset cifar100 --data_dir ./../../../data/cifar100 --local_rank 0 --task_specific_layer_type 12
+python ./main_vit_frozen.py --lr 0.03 --dataset cifar100 --data_dir ./../../../data/cifar100 --local_rank 2 --task_specific_layer_type 12
 
-python ./main_vit_frozen.py --lr 0.01 --dataset cifar100 --data_dir ./../../../data/cifar100 --local_rank 0 --task_specific_layer_type 12
+python ./main_vit_frozen.py --lr 0.01 --dataset cifar100 --data_dir ./../../../data/cifar100 --local_rank 2 --task_specific_layer_type 12
 
 # 0.8431, 0.8645
-python ./main_vit_frozen.py --lr 0.003 --dataset cifar100 --data_dir ./../../../data/cifar100 --local_rank 0 --task_specific_layer_type 12
-python ./main_vit_frozen.py --lr 0.001 --dataset cifar100 --data_dir ./../../../data/cifar100 --local_rank 0 --task_specific_layer_type 12
+python ./main_vit_frozen.py --lr 0.003 --dataset cifar100 --data_dir ./../../../data/cifar100 --local_rank 2 --task_specific_layer_type 12
+python ./main_vit_frozen.py --lr 0.001 --dataset cifar100 --data_dir ./../../../data/cifar100 --local_rank 2 --task_specific_layer_type 12
 
-python ./main_vit_frozen.py --lr 0.0003 --dataset cifar100 --data_dir ./../../../data/cifar100 --local_rank 0 --task_specific_layer_type 12
+python ./main_vit_frozen.py --lr 0.0003 --dataset cifar100 --data_dir ./../../../data/cifar100 --local_rank 2 --task_specific_layer_type 12
 
-python ./main_vit_frozen.py --lr 0.0001 --dataset cifar100 --data_dir ./../../../data/cifar100 --local_rank 0 --task_specific_layer_type 12
+python ./main_vit_frozen.py --lr 0.0001 --dataset cifar100 --data_dir ./../../../data/cifar100 --local_rank 2 --task_specific_layer_type 12
